e8a2d063e11f84452e3284330c9c3594b7c94159
ntsinh <ntsinh151@gmail.com>
2016-09-23 09:43:34 +0700

    update v1.1
    thêm style button,text

--- src/screenoption/MainActivity.java
+++ src/screenoption/MainActivity.java
@@ -5,8 +5,6 @@ import about_me.infomation;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
